b5ca4d406c3e5b4558ba1a460feeed1c66519000
unknown <akshaymadiwalar@gmail.com>
2022-03-20 17:36:07 -0700

    ip change

--- server/src/controllers/CartController.js
+++ server/src/controllers/CartController.js
@@ -5,11 +5,13 @@ exports.addToCart = (req,res) => {
     const {productId,userId,quantity,price} = req.body
 
     ProductModel.findByProductId({productId},(err,data)=>{
+        console.log(err)
         if(err) return res.status(500).json({message:'Server Error'})
 
         if(data.length > 0){
             const {elastic_id,seller_id,product_name,img,category,description} = data[0]
             CartModel.addToCart({elastic_id,productId,userId,sellerId:seller_id,productName:product_name,img,category,description,price,quantity},(err,data)=>{
+                console.log(err)
                 if(err) return res.status(500).json({message:'Server error'})
                 if(data){
                     return res.json({message:'Added to Cart'})
